4559fdd9cd840b0b115e11a67227a757380ea780
AdamRB208 <adamrborup@gmail.com>
2025-03-03 16:44:14 -0700

    updated service and appstate to map pojo

--- app/AppState.js
+++ app/AppState.js
@@ -6,7 +6,7 @@ class ObservableAppState extends EventEmitter {
   /** 
      * @type {TriviaModel[]} 
   */
-  TriviaModel = []
+  triviaModels = []
 
 }
 
--- app/services/TriviaService.js
+++ app/services/TriviaService.js
@@ -1,3 +1,6 @@
+import { AppState } from "../AppState.js"
+import { TriviaModel } from "../models/TriviaModel.js"
+
 // @ts-ignore
 const triviaServiceAPI = axios.create({
   baseURL: 'https://opentdb.com/api.php?amount=10&category=9&type=multiple',
@@ -9,7 +12,11 @@ class TriviaService {
   async getQuestions() {
 
     const response = await triviaServiceAPI.get('https://opentdb.com/api.php?amount=10&category=9&type=multiple')
-    console.log('response', response)
+    console.log('received trivia questions', response.data)
+
+    const triviaModels = response.data.results.map(pojo => new TriviaModel(pojo))
+    AppState.triviaModels = triviaModels
+    console.log('questions!', AppState.triviaModels)
   }
 }
 
