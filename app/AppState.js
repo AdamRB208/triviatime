32ab5aca10330932855354c6d79ae2b88c84e5b3
AdamRB208 <adamrborup@gmail.com>
2025-03-03 16:23:27 -0700

    imported Model in AppState

--- app/AppState.js
+++ app/AppState.js
@@ -1,8 +1,12 @@
+import { TriviaModel } from './models/TriviaModel.js'
 import { EventEmitter } from './utils/EventEmitter.js'
 import { createObservableProxy } from './utils/ObservableProxy.js'
 
 class ObservableAppState extends EventEmitter {
-
+  /** 
+     * @type {TriviaModel[]} 
+  */
+  TriviaModel = []
 
 }
 
